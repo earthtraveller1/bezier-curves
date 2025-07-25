e512a3c9df652f7861bc7b1beb07aa88496e3f5f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2025-07-25 16:00:23 -0400

    Drew some more shapes and stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,13 @@ int main() {
     while (window.is_open()) {
         graphics::begin();
 
-        graphics::draw_circle(100.0, 100.0, 50.0);
+        graphics::draw_circle(100.0, 100.0, 10.0);
+        graphics::draw_circle(150.0, 100.0, 10.0);
+        graphics::draw_circle(200.0, 100.0, 10.0);
+        graphics::draw_circle(250.0, 100.0, 10.0);
+        graphics::draw_circle(300.0, 100.0, 10.0);
+
+        graphics::draw_line(100.0, 100.0, 300.0, 100.0, 5.0);
 
         graphics::end();
         window.update();
